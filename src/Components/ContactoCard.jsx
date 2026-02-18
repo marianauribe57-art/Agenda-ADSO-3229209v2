639fbd36e55fe5e2dfe5a294c0d2f9571e5cd709
Mariana Uribe <marianauribe57@gmail.com>
2026-02-18 16:16:48 -0500

    feat(Components/ContactoCard.jsx) Actualización clase 4

--- src/Components/ContactoCard.jsx
+++ src/Components/ContactoCard.jsx
@@ -2,24 +2,53 @@
 // Este componente muestra UN contacto de la agenda.
 // Recibe datos (props): nombre, telefono, correo, etiqueta.
 
-export default function ContactoCard({ nombre, telefono, correo, etiqueta }) {
+/*export default function ContactoCard({ nombre, telefono, correo, etiqueta }) {
   return (
     <div className="card-contacto">
-      {/* Nombre del contacto en negrita */}
-      <h3 className="card-nombre">{nombre}</h3>
+      /* Nombre del contacto en negrita */
+      /*<h3 className="card-nombre">{nombre}</h3>
 
-      {/* Teléfono */}
-      <p className="card-linea">📞 {telefono}</p>
+      /* Teléfono */
+      /*<p className="card-linea">📞 {telefono}</p>
+        /* Correo */
+      //<p className="card-linea">📧 {correo}</p>
 
-      {/* Correo */}
-      <p className="card-linea">📧 {correo}</p>
+      /* Etiqueta adicional, si existe */
+      //{etiqueta && (
+        //<p className="card-etiqueta">{etiqueta}</p>
+      //)}
+    //</div>
+  //);*/
 
-      {/* Etiqueta adicional, si existe */}
-      {etiqueta && (
-        <p className="card-etiqueta">{etiqueta}</p>
+  // src/components/ContactoCard.jsx
 
+export default function ContactoCard({
+  id,
+  nombre,
+  telefono,
+  correo,
+  etiqueta,
+  onDelete,
+}) {
+  return (
+    <article className="tarjeta-contacto">
+      <h3>{nombre}</h3>
 
-      )}
-    </div>
+      {etiqueta && <p className="tag">{etiqueta}</p>}
+
+      <p>📞 {telefono}</p>
+
+      {correo && <p>✉️ {correo}</p>}
+
+      <div className="acciones">
+        <button
+          type="button"
+          className="btn-eliminar"
+          onClick={() => onDelete(id)}
+        >
+          Eliminar
+        </button>
+      </div>
+    </article>
   );
 }
